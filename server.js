dc90eaed3a022c3d839063586d12a08271692d17
sundarR-developer <sundarravi0723@gmail.com>
2025-08-01 15:27:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,20 +16,13 @@ app.use(express.json());
 // Define allowed origins
 const allowedOrigins = [
   'http://localhost:3000',
-  'https://unrivaled-lamington-8daa84.netlify.app'
+  'https://unrivaled-lamington-8daa84.netlify.app',
+  'https://quiz-server-9.onrender.com'
 ];
 
 // CORS configuration
 const corsOptions = {
-  origin: (origin, callback) => {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  },
+  origin: allowedOrigins,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD'],
   credentials: true,
 };
