be1fe661bef421c4ed7b1ae7103d0bda7db7f775
sundarR-developer <sundarravi0723@gmail.com>
2025-08-03 00:29:46 +0530

    Update userSchema.js

--- models/userSchema.js
+++ models/userSchema.js
@@ -8,17 +8,11 @@ const userSchema = new mongoose.Schema({
   role: { type: String, enum: ['student', 'admin', 'proctor'], default: 'student' }
 });
 
-// ✅ Hash password before saving
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// ✅ Add this method for login
-userSchema.methods.matchPassword = async function(enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
 const User = mongoose.model('User', userSchema);
-export default User;
+export default User; 
